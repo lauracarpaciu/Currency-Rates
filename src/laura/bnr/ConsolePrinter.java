47a83e8da27bf76b9a7d73139fc43f5ccde3c1c0
Laura Carpaciu <mirelacarpaciu@yahoo.com>
2017-10-27 10:27:06 +0300

    add comments to cod

--- src/laura/bnr/ConsolePrinter.java
+++ src/laura/bnr/ConsolePrinter.java
@@ -1,20 +1,23 @@
 package laura.bnr;
 
-import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class ConsolePrinter implements CurrencyPrinter {
+//    create a print method for printing on console
     @Override
     public void print(ArrayList<Currency> currencies) {
+//        create date object
+
         LocalDate date = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/YYYY");
-
+// obtain an object with String type
         String text = date.format(formatter);
 
         String header = String.format("%s: %s", "#BNR - Reference rates", text);
         System.out.println(header);
+//        create a foreach loop and print the result
         for (Currency currency : currencies) {
             System.out.println(currency.getDescription());
         }
