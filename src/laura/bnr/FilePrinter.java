1c0f3a9a72d5cc103808dd14d46c03bf4de047dc
Laura Carpaciu <mirelacarpaciu@yahoo.com>
2017-10-27 10:27:41 +0300

    add comments to cod

--- src/laura/bnr/FilePrinter.java
+++ src/laura/bnr/FilePrinter.java
@@ -1,12 +1,8 @@
 package laura.bnr;
 
-import laura.bnr.Currency;
-import laura.bnr.CurrencyPrinter;
-
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -14,37 +10,35 @@ import java.io.IOException;
 public class FilePrinter implements CurrencyPrinter {
     @Override
     public void print(ArrayList<Currency> currencies) {
+// use BufferedWrriter class to print in file
         BufferedWriter bufferedWriter = null;
-
-        try {
-            bufferedWriter = new BufferedWriter(new FileWriter("BNR.proprieties"));
-
-
-            LocalDate date = LocalDate.now();
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/YYYY");
-            String text = date.format(formatter);
-
-            String header = String.format("%s: %s", "#BNR - Reference rates", text);
-            bufferedWriter.write(header);
-            bufferedWriter.newLine();
-
-            for (Currency currency : currencies) {
-                bufferedWriter.write(currency.getDescription());
-                bufferedWriter.newLine();
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-
-        } finally {
-            //Close the BufferedWriter
+        {
             try {
-                if (bufferedWriter != null) {
-                    bufferedWriter.flush();
-                    bufferedWriter.close();
+                bufferedWriter = new BufferedWriter(new FileWriter("BNR.proprieties"));
+                LocalDate date = LocalDate.now();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/YYYY");
+                String text = date.format(formatter);
+                String header = String.format("%s: %s", "#BNR - Reference rates", text);
+                bufferedWriter.write(header);
+                bufferedWriter.newLine();
+//create a foreach loop; and print with write method of bufferedWrither object;
+                for (Currency currency : currencies) {
+                    bufferedWriter.write(currency.getDescription());
+                    bufferedWriter.newLine();
                 }
-            } catch (IOException ex) {
-                ex.printStackTrace();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            } finally {
+                try {
+                    if (bufferedWriter != null) {
+                        bufferedWriter.flush();
+                        bufferedWriter.close();
+                    }
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
             }
         }
     }
